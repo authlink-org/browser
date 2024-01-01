247ae5d4e89fff2fe9b5e7af8c79b64e7f100bb5
BartekSadurski <148957368+BartekSadurski@users.noreply.github.com>
2024-01-01 17:47:19 +0100

    Fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/authlink/navbar";
 import Header from "@/components/authlink/header";
 
 export const metadata = {
-  title: "Browser top project - AuthLink.org",
+  title: "Browse top project - AuthLink.org",
   description:
     "View newly created projects, search for them, and dicover new software.",
 };
