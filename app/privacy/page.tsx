84d2a0d05235ebc50fbb87ed9eae1fb37d5ba2dd
BartekSadurski <148957368+BartekSadurski@users.noreply.github.com>
2024-01-01 20:18:12 +0100

    Add privacy policy

---
+++ app/privacy/page.tsx
@@ -0,0 +1,70 @@
+export default function Privacy() {
+  return (
+    <div className="break-after-auto container mx-auto mt-36">
+      At AuthLink.org, we respect your privacy and are committed to safeguarding
+      any personal information you provide when visiting our website. This
+      Privacy Policy outlines how we collect, use, store, and disclose your
+      information. By accessing AuthLink.org, you consent to the practices
+      described in this policy.
+      <br />
+      <b>Information Collected</b>
+      <br />
+      When you visit AuthLink.org, we collect the following information: User IP
+      Address: We log and store the IP address of each visitor to AuthLink.org.
+      This helps us understand traffic patterns and diagnose technical issues.
+      Time of Visit: We record the time and date of your visit to AuthLink.org
+      for analytical purposes and to enhance user experience. Country Code: We
+      capture the country code of visitors to better understand our global
+      audience and comply with regional regulations. Project Visited: We track
+      the specific project you accessed on our website. This information assists
+      us in improving our content and tailoring our services to your needs.
+      <br />
+      <b>Use of Information</b>
+      <br />
+      The data collected is used for the following purposes: Website
+      Improvement: We analyze the gathered information to enhance our website's
+      content, functionality, and user experience. Analytics: We use analytics
+      tools to track user behavior, understand trends, and optimize our
+      services. Compliance and Security: We may use the collected data to comply
+      with legal obligations and to ensure the security of our website and its
+      <br />
+      users.
+      <br />
+      <b>Third-Party Providers</b>
+      <br />
+      AuthLink.org utilizes various third-party service providers, including but
+      not limited to Google AdSense. These providers may collect additional
+      information about your visit to our website, as governed by their
+      respective privacy policies. We do not have control over the data
+      collection practices of these third-party services and encourage you to
+      review their privacy policies for more information.
+      <br />
+      <b>Data Security</b>
+      <br />
+      We employ industry-standard security measures to protect your personal
+      information from unauthorized access, alteration, disclosure, or
+      destruction. However, no method of data transmission over the internet or
+      electronic storage is completely secure, and we cannot guarantee absolute
+      security.
+      <br />
+      <b>Updates to Privacy Policy</b>
+      <br />
+      We reserve the right to update and modify this Privacy Policy at any time.
+      Any changes will be effective immediately upon posting the updated policy
+      on AuthLink.org. Your continued use of our website after such
+      modifications constitutes acceptance of the updated Privacy Policy.
+      <br />
+      <b>Contact Us</b>
+      <br />
+      If you have any questions, concerns, or requests regarding this Privacy
+      Policy or the data practices of AuthLink.org, please contact us at
+      contact@authlink.org. This Privacy Policy applies solely to information
+      collected by AuthLink.org and does not cover information collected by any
+      other website or service linked to or from AuthLink.org.
+      <br />
+      <br />
+      Last Updated: <b>01.01.2024 8:16 PM</b>
+      <br />
+    </div>
+  );
+}
