162948975d338416c8526bba9743485ec3a2d41e
BartekSadurski <148957368+BartekSadurski@users.noreply.github.com>
2024-01-15 13:46:03 +0100

    Refresh top projects?

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Navbar from "@/components/authlink/navbar";
 import Header from "@/components/authlink/header";
 import Info from "@/components/authlink/info";
