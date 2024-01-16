88a8a41eda3200547035af5fedde9f7d27fb5343
BartekSadurski <148957368+BartekSadurski@users.noreply.github.com>
2024-01-16 15:37:38 +0100

    Add consent

--- components/authlink/info.tsx
+++ components/authlink/info.tsx
@@ -1,12 +1,30 @@
+import Script from "next/script";
+
 export default function Info() {
   return (
     <div className="container mx-auto flex justify-center gap-4 mb-4">
-      <a
+      {/* <a
         href="/privacy"
         className="hover:underline underline-offset-8 decoration-wavy decoration-green-500"
       >
         Privacy
+      </a> */}
+      <a
+        href="https://www.iubenda.com/privacy-policy/57438358"
+        className="hover:underline underline-offset-8 decoration-wavy decoration-green-500"
+        title="Privacy Policy "
+      >
+        Privacy Policy
       </a>
+      <Script src="/privacy.js" />
+      <a
+        href="https://www.iubenda.com/privacy-policy/57438358/cookie-policy"
+        className="hover:underline underline-offset-8 decoration-wavy decoration-green-500"
+        title="Cookie Policy "
+      >
+        Cookie Policy
+      </a>
+      <Script src="/cookie.js" />
       <a
         href="/projects"
         className="hover:underline underline-offset-8 decoration-wavy decoration-green-500"
